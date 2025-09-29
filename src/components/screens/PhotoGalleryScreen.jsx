247140821d5addc5f639f32deaf962e767cfe764
drake <drakeopdf@gmail.com>
2025-09-30 05:13:29 +0530

    Update PhotoGalleryScreen.jsx

--- src/components/screens/PhotoGalleryScreen.jsx
+++ src/components/screens/PhotoGalleryScreen.jsx
@@ -15,6 +15,9 @@ export default function PhotoGalleryScreen({ onNext }) {
         "/images/3.jpg",
         "/images/4.jpg",
         "/images/5.jpg",
+        "/images/6.jpg",
+        "/images/7.jpg",
+        "/images/8.jpg",
     ]
 
     const [details, setDetails] = React.useState(null)
